c5be9740836fa4dff768bf007d3085388ab61a37
Gean Ferreira <geanoficial14@gmail.com>
2025-11-17 06:09:20 -0300

    feat : adicionando a seção de insights na página de informações de um equipamento

--- ativos/views.py
+++ ativos/views.py
@@ -18,36 +18,81 @@ def equipamento(request):
         "data_aquisicao": "15/07/2022",
         "data_garantia": "15/07/2025",
         "data_ultima_manutencao": "08/10/2024",
+        "status": "defeituoso",
         "historico_manutencoes": [
             {
                 "manutencao_id": "TASK-8782",
                 "titulo": "Equipamento com mau funcionamento na fonte",
-                "status": "Em andamento",
+                "responsavel": "Alexandre Magno",
                 "data": "08/10/2024"
             },
             {
                 "manutencao_id": "TASK-8782",
                 "titulo": "Equipamento com suspeita de vírus",
-                "status": "Cancelada",
+                "responsavel": "Maximus Decimos Meridios",
                 "data": "25/10/2020"
             },
             {
                 "manutencao_id": "TASK-8782",
                 "titulo": "Equipamento com vírus em seu sistema operacional",
-                "status": "Concluída",
+                "responsavel": "Carlos Magno",
                 "data": "02/10/2020"
             },
             {
                 "manutencao_id": "TASK-8782",
                 "titulo": "Falha no cabo HDMI – substituição completa",
-                "status": "Concluída",
+                "responsavel": "Tales de Mileto",
                 "data": "02/10/2020"
             },
             {
                 "manutencao_id": "TASK-8782",
                 "titulo": "Revisão de rotina sem anomalias",
-                "status": "Concluída",
+                "responsavel": "Carlos Drummond de Andrade",
                 "data": "02/10/2020"
+            },
+            {
+                "manutencao_id": "TASK-8782",
+                "titulo": "Equipamento com suspeita de vírus",
+                "responsavel": "Maximus Decimos Meridios",
+                "data": "25/10/2020"
+            },
+            {
+                "manutencao_id": "TASK-8782",
+                "titulo": "Equipamento com vírus em seu sistema operacional",
+                "responsavel": "Carlos Magno",
+                "data": "02/10/2020"
+            }
+        ],
+        "insights": [
+            {
+                "titulo": "Confiabilidade",
+                "status": "ruim",
+                "valor": "10%",
+            },
+            {
+                "titulo": "Ultima verificação",
+                "status": "ruim",
+                "valor": "100 dias atrás",
+            },
+            {
+                "titulo": "Tempo médio entre falhas",
+                "status": "preocupante",
+                "valor": "40 dias",
+            },
+            {
+                "titulo": "Tendência de falhas",
+                "status": "ruim",
+                "valor": "Alta",
+            },
+            {
+                "titulo": "Tempo médio de reparo",
+                "status": "bom",
+                "valor": "1h10",
+            },
+            {
+                "titulo": "Confiabilidade",
+                "status": "ruim",
+                "valor": "10%",
             }
         ]
     }
