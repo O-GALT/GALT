b89893e0ca0e3295e5b78e563c14a5e7942bf2aa
Lucas Jales <lucasjales990@gmail.com>
2025-11-17 22:49:48 -0300

    endpoint da tela de criacao de recursos

--- contas/urls.py
+++ contas/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='contas_index'),
     path('@eu/', views.eu, name='contas_eu'),
     # URL QUE CRIEI APENAS PARA TESTAR A RENDERIZAÇÃO DO HEADER DE CRIACAO DE RECURSOS (se quiser, pode excluir)
-    # path('criar-recursos/', views.criar_recursos, name='criar_recursos'),
+    path('criar-recursos/', views.criar_recursos, name='criar_recursos'),
 ]
