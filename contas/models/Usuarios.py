8d81de85eb5aa870103245be33d59d9dfb8517bc
Lucas Jales <lucasjales990@gmail.com>
2026-01-02 21:02:56 -0300

    fix: responsividade da pagina de login ao ficar com 803 pixels corrigida

--- contas/models/Usuarios.py
+++ contas/models/Usuarios.py
@@ -1,16 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from core.essenciais import TipoUsuario
 
-class Usuarios(AbstractUser):
+class Usuario(AbstractUser):
     nome = models.CharField(null=False, blank=False, max_length=100)
     email_pessoal = models.CharField(null=False, blank=False, max_length=100)
     email_escolar = models.CharField(null=False, blank=False, max_length=100)
-    cpf = models.CharField(null=False, blank=False, max_length=14)
-    numero = models.CharField(null=False, blank=False, max_length=13)
+    cpf = models.CharField(null=False, blank=False, max_length=13)
+    telefone = models.CharField(null=False, blank=False, max_length=13)
+    tipo_usuari = models.CharField(null=False, blank=False, max_length=100, choices=TipoUsuario.choices)
 
-    class Meta:
-        verbose_name = 'Usuarios'
-        verbose_name_plural = 'Usuarios'
+class Meta:
 
-    def __str__(self):
-        return self.nome
+    verbose_name = 'Usuario'
+    verbose_name_plural = 'Usuarios'
+
+def __str__(self):
+    return self.nome 
