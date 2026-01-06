02a4479d5499887c0c146c9af87abb4213ef331d
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2026-01-06 19:31:41 -0300

    refactor: refatorar tipos de usuarios

--- core/essenciais/TipoUsuario.py
+++ core/essenciais/TipoUsuario.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class TipoUsuario(models.TextChoices):
-    PROFESSOR = 'PROFESSOR', 'Professor'
-    ALUNO = 'ALUNO', 'Aluno'
-    TECNICO_TI = 'TECNICO_TI', 'Tecnico de TI'
     ADMINISTRADOR = 'ADMINISTRADOR', 'Administrador'
-    ROOT = 'ROOT', 'Root'
+    TECNICO_TI = 'TECNICO_TI', 'Tecnico de TI'
+    ALUNO = 'ALUNO', 'Aluno'
+    SERVIDOR = 'SERVIDOR', 'Servidor'
