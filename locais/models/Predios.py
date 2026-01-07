27dad5ca2c1079dded2d26e9b8765fa3b29b66ca
Lucas Jales <lucasjales990@gmail.com>
2026-01-07 16:07:05 -0300

    resolvido

--- locais/models/Predios.py
+++ locais/models/Predios.py
@@ -3,6 +3,11 @@ from django.db import models
 class Predios(models.Model):
     predio_id = models.AutoField(primary_key=True)
     predio = models.CharField(null=False, blank=False, unique=True, max_length=30)
+<<<<<<< HEAD:locais/models/Predios.py
+    predio = models.CharField(null=False, blank=False, unique=True, max_length=100)
+=======
+    predio = models.CharField(null=False, blank=False, unique=True, max_length=30)
+>>>>>>> 55402a8 (feat: verificação de senha nativa do AbstractUser):locais/models/Predio.py
 
     class Meta:
         verbose_name = 'Predio'
