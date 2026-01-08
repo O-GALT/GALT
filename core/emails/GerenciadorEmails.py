81c6f05323d9ef680e457535b1818b0fadf0bea4
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2026-01-08 19:50:10 -0300

    refactor: ajustar envio de emails

--- core/emails/GerenciadorEmails.py
+++ core/emails/GerenciadorEmails.py
@@ -2,22 +2,19 @@ import smtplib
 import  os
 from email.message import EmailMessage
 
-from core.essenciais import TipoUsuario
-
 EMAIL = os.getenv('EMAIL')
 SENHA = os.getenv('SENHA')
 
-
 class GerenciadorEmails:
-    def __init__(self):
-        self.mensagem = EmailMessage()
-        self.mensagem['Subject'] = 'Conta criada no GALT'
-        self.mensagem['From'] = EMAIL
+    @staticmethod
+    def enviar_email(email_pessoal:str, email_escolar:str, senha:str, tipo_usuario):
+        mensagem = EmailMessage()
+        mensagem['Subject'] = 'Conta criada no GALT'
+        mensagem['From'] = EMAIL
 
-    def enviar_email(self, email_pessoal:str, email_escolar:str, senha:str, tipo_usuario: TipoUsuario):
-        self.mensagem['To'] = email_pessoal
-        self.mensagem.set_content(f'Acesso autorizado para o sistema GALT\nconta criada para {tipo_usuario}\nCredenciais:\nEmail: {email_escolar}\nSenha: {senha}')
+        mensagem['To'] = email_pessoal
+        mensagem.set_content(f'Acesso autorizado para o sistema GALT\nconta criada com nível de acesso de: {[tipo.name for tipo in tipo_usuario.all()]}\nCredenciais:\nEmail: {email_escolar}\nSenha: {senha}')
 
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
             smtp.login(EMAIL, SENHA)
-            smtp.send_message(self.mensagem)
+            smtp.send_message(mensagem)
