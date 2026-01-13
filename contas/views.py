b4e5b5694de1f4a44c5a675bd488d7e186742abe
Lucas Jales <lucasjales990@gmail.com>
2026-01-12 23:02:34 -0300

    alteraçoe a continuar

--- contas/views.py
+++ contas/views.py
@@ -1,12 +1,21 @@
+from django.contrib.auth.decorators import login_required
+from core.autorizacao.filtroAutorizacao import nivel_acesso_permitido
+from core.essenciais import TipoUsuario
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+@login_required
+@nivel_acesso_permitido([TipoUsuario.ADMINISTRADOR, TipoUsuario.TECNICO_TI, TipoUsuario.ALUNO]) 
 def index(request):
     return HttpResponse("Página de contas funcionando!")
 
+@login_required
 def eu(request):
     return render(request, 'contas/partials/components_centrais/informacoes_pessoais/informacoes_pessoais.html')
 # VIEW QUE CRIEI PARA TESTAR A RENDERIZACAO DO HEADER DA PAGINA DE CRIACAO DE RECURSOS(se quiser, pode excluir)
+@login_required
+@nivel_acesso_permitido([TipoUsuario.ADMINISTRADOR, TipoUsuario.TECNICO_TI])
 def criar_recursos(request):
     return render(request,'contas/partials/components_centrais/recursos/recursos.html')
