d7149950bc796610663150fc4f970cfae42c05b1
Gean Ferreira <geanoficial14@gmail.com>
2026-01-13 16:34:54 -0300

    refactor : criar o componente que representa um card do kanban e integraliza-lo

--- agendas/views.py
+++ agendas/views.py
@@ -4,4 +4,81 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'agendas/partials/kanban/kanban.html')
+    context = {}
+    context['pendentes'] = {
+        "cards" : [
+            {
+                "date": "25/01/2026",
+                "nome_sala": "Sala A17",
+                "nome_setor": "Setor de salas de aulas",
+                "nome_predio": "Prédio principal",
+                "hora_inicio": "09:10",
+                "hora_fim": "10:30",
+                "numero_tecnicos": 5,
+            },
+        ],
+        "count" : 2
+    }
+
+    context['em_andamento'] = {
+        "cards" : [
+            {
+                "date": "25/01/2026",
+                "nome_sala": "Sala A17",
+                "nome_setor": "Setor de salas de aulas",
+                "nome_predio": "Prédio principal",
+                "hora_inicio": "09:10",
+                "hora_fim": "10:30",
+                "numero_tecnicos": 5,
+            },
+        ],
+        "count" : 2
+    }
+
+    context['feedback'] = {
+        "cards" : [
+            {
+                "date": "25/01/2026",
+                "nome_sala": "Sala A17",
+                "nome_setor": "Setor de salas de aulas",
+                "nome_predio": "Prédio principal",
+                "hora_inicio": "09:10",
+                "hora_fim": "10:30",
+                "numero_tecnicos": 5,
+            },
+        ],
+        "count" : 2
+    }
+
+    context['inacabado'] = {
+        "cards" : [
+            {
+                "date": "25/01/2026",
+                "nome_sala": "Sala A17",
+                "nome_setor": "Setor de salas de aulas",
+                "nome_predio": "Prédio principal",
+                "hora_inicio": "09:10",
+                "hora_fim": "10:30",
+                "numero_tecnicos": 5,
+            },
+        ],
+        "count" : 2
+    }
+
+    context['finalizado'] = {
+        "cards" : [
+            {
+                "date": "25/01/2026",
+                "nome_sala": "Sala A17",
+                "nome_setor": "Setor de salas de aulas",
+                "nome_predio": "Prédio principal",
+                "hora_inicio": "09:10",
+                "hora_fim": "10:30",
+                "numero_tecnicos": 5,
+            },
+        ],
+        "count" : 2
+    }
+
+
+    return render(request, 'agendas/pages/kanban/kanban.html' , context)
