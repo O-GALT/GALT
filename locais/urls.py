9b606dbf95922760da623c85a6fc5848a1353bf6
allana-clarice <antoniaallana536@gmail.com>
2026-01-15 15:12:46 -0300

    implementando ajustes para urls e views.

--- locais/urls.py
+++ locais/urls.py
@@ -4,12 +4,12 @@ from .views import predios_equipamentos
 
 urlpatterns = [
     path('', views.index, name='locais_index'),
-    path('predios/<int:predio_id>/', views.predios, name='predio_detail'),
-    path('predios/<int:predio_id>/setores/', views.predios_setores, name='predio_setores'),
-    path('predios/<int:predio_id>/salas/', views.predios_salas, name='predio_salas'),
-    path('predios/<int:predio_id>/equipamentos/', views.predios_equipamentos, name='predio_equipamentos'),
-    path('setores/<int:setor_id>/', views.setores, name='setor_detail'),
-    path('salas/<int:sala_id>/', views.salas, name='sala_detail'),
+    path('predios/<int:predio_id>/', views.predios, name='locais_predio_detail'),
+    path('predios/<int:predio_id>/setores/', views.predios_setores, name='locais_predio_setores'),
+    path('predios/<int:predio_id>/salas/', views.predios_salas, name='locais_predio_salas'),
+    path('predios/<int:predio_id>/equipamentos/', views.predios_equipamentos, name='locais_predio_equipamentos'),
+    path('setores/<int:setor_id>/', views.setores, name='locais_setor_detail'),
+    path('salas/<int:sala_id>/', views.salas, name='locais_sala_detail'),
 
     # URL'S QUE USEI DE TESTE PARA TESTAR RENDERIZAÇÃO DA SELECAO DE EQUIPAMENTOS E SALAS NA PARTE DO PREDIO PRINCIPAL(se quiser, pode excluir)
     # path('predios/equipamentos/', views.predios_equipamentos, name='predios_equipamentos'),
