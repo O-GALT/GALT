f7aa18472671d7b5534eb8d4f15ec54257f0e7f6
Lucas Jales <lucasjales990@gmail.com>
2026-01-24 16:45:27 -0300

    feat: removidos camppos nome e cargo, adicionado mudar dados da pagina de perfil

--- contas/views.py
+++ contas/views.py
@@ -13,10 +13,15 @@ def index(request):
 @nivel_acesso_permitido([TipoUsuario.ADMINISTRADOR, TipoUsuario.TECNICO_TI, TipoUsuario.ALUNO, TipoUsuario.SERVIDOR]) 
 def eu(request):
     usuario = request.user
-    context = {
-        'usuario': usuario
-    }
-    return render(request, 'contas/partials/components_centrais/informacoes_pessoais/informacoes_pessoais.html', context)
+
+    if request.method == "POST":
+        usuario.cpf = request.POST.get("cpf")
+        usuario.email = request.POST.get("email")
+        usuario.numero = request.POST.get("numero")
+        usuario.email_escolar = request.POST.get("email_escolar")
+        usuario.save()
+
+    return render(request, 'contas/partials/components_centrais/informacoes_pessoais/informacoes_pessoais.html', {'usuario': usuario})
 
 @login_required
 @nivel_acesso_permitido([TipoUsuario.ADMINISTRADOR])
