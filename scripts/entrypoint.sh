cd4764d062ab8a236d5f4b1e2b9a11675919f669
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2026-01-26 05:38:35 -0300

    ajuste

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,8 +4,7 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-celery -A GALT worker -l info > logs/celery_worker.log 2>&1 &
-celery -A GALT beat -l info > logs/celery_beat.log 2>&1 &
+mkdir logs
 
 exec gunicorn GALT.wsgi:application \
   --bind 0.0.0.0:8000 \
